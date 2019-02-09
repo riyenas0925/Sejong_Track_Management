b1be6a9a097a29574f92d074414321307716a0a3
Dongmin Kang <riyenas0925@gmail.com>
2019-02-09 19:58:26 +0900

    CLN:testVO 삭제, ruleVO와 연결

--- SejongTrack/src/main/java/kr/ac/sejong/persistence/UploadResultDAO.java
+++ SejongTrack/src/main/java/kr/ac/sejong/persistence/UploadResultDAO.java
@@ -1,13 +1,13 @@
 package kr.ac.sejong.persistence;
 
-import kr.ac.sejong.domain.testVO;
+import kr.ac.sejong.domain.ruleVO;
 import kr.ac.sejong.domain.trackSubjectVO;
 
 import java.util.List;
 
 
 public interface UploadResultDAO {
-    public testVO readRule(Integer ruleNo)throws Exception;
+    public ruleVO readRule(Integer ruleNo)throws Exception;
 
     public List<trackSubjectVO> readSub(Integer trackNo)throws Exception;
 
--- SejongTrack/src/main/java/kr/ac/sejong/service/UploadResultService.java
+++ SejongTrack/src/main/java/kr/ac/sejong/service/UploadResultService.java
@@ -1,14 +1,13 @@
 package kr.ac.sejong.service;
 
-import kr.ac.sejong.domain.testVO;
+import kr.ac.sejong.domain.ruleVO;
 import kr.ac.sejong.domain.trackSubjectVO;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 
 public interface UploadResultService {
-    public testVO readRule(Integer ruleNo)throws Exception;
+    public ruleVO readRule(Integer ruleNo)throws Exception;
 
     public List<trackSubjectVO> readSub(Integer subType)throws Exception;
 
