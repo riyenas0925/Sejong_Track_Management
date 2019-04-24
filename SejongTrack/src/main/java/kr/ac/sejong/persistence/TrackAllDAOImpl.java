8da83bc9cfe03131adc5cb1ef8f936c19eceba85
Dongmin Kang <riyenas0925@gmail.com>
2019-04-25 03:07:18 +0900

    test

--- SejongTrack/src/main/java/kr/ac/sejong/persistence/TrackAllDAOImpl.java
+++ SejongTrack/src/main/java/kr/ac/sejong/persistence/TrackAllDAOImpl.java
@@ -20,6 +20,6 @@ public class TrackAllDAOImpl implements  TrackAllDAO{
 
     @Override
     public List<trackAllVO> trackAllList(Integer univNo) throws Exception{
-        return session.selectList(namespace+".trackAllListTest", univNo);
+        return session.selectList(namespace+".trackAllList", univNo);
     }
 }
