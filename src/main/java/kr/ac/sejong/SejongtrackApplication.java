323cb3e2eb47b3cfd9e6f5bd581ec3657b09c4d9
riyenas0925 <riyenas0925@gmail.com>
2020-05-21 22:59:33 +0900

    ENH : Jenkins용 kill_process.sh 추가, 배포 방식 jar에서 war로 변경

--- src/main/java/kr/ac/sejong/SejongtrackApplication.java
+++ src/main/java/kr/ac/sejong/SejongtrackApplication.java
@@ -2,11 +2,20 @@ package kr.ac.sejong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @EnableCaching
 @SpringBootApplication
-public class SejongtrackApplication {
+public class SejongtrackApplication extends SpringBootServletInitializer {
+
+    //Test
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SejongtrackApplication.class);
+    }
+
     public static void main(String[] args) {
 
         String profile = System.getProperty("spring.profiles.active");
